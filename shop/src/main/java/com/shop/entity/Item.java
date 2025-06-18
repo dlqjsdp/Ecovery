105673f7e59643c7cfee0b862fab66a9c6e848cd
Leenavi <tlsrl930@gmail.com>
2025-06-18 11:35:57 +0900

    20250618

---
+++ shop/src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,41 @@
+package com.shop.entity;
+
+import com.shop.constant.ItemSellStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="item")
+@Getter
+@Setter  //entity에 setter는 좋은 건 아님.
+@ToString
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //상품 코드
+
+    @Column(nullable = false, length = 50)
+    private String itemNm; //상품명
+
+    @Column(nullable = false, name="price")
+    private int price; //상품 가격
+
+    @Column(nullable = false)
+    private int stockNumber; //재고 수량
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail; //상품 상세 설명
+
+    @Enumerated(EnumType.STRING)  //string 필수
+    private ItemSellStatus itemSellStatus; //상품 판매 상태
+
+    private LocalDateTime regTime; //등록 시간
+
+    private LocalDateTime updateTime; //수정 시간
+}
