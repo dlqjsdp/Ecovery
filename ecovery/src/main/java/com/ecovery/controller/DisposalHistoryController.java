04a35e65f1c07f375252cb948272820709113d07
Lee <tlsrl930@gmail.com>
2025-07-20 15:54:43 +0900

    추가 안되는 게 있음

--- ecovery/src/main/java/com/ecovery/controller/DisposalHistoryController.java
+++ ecovery/src/main/java/com/ecovery/controller/DisposalHistoryController.java
@@ -20,15 +20,31 @@ public class DisposalHistoryController {
 
     private final DisposalHistoryService disposalHistoryService;
 
-    @GetMapping("")
+    @GetMapping("/disposalMain")
     public String disposalMain() {
         return "/disposal/disposalMain";
     }
 
     @GetMapping("/{disposalHistoryId}")
     public String disposalHistory(@PathVariable("disposalHistoryId") Long disposalHistoryId, Model model) {
-        DisposalHistoryDto disposalHistory = disposalHistoryService.getHistory(disposalHistoryId);
-        model.addAttribute("disposalHistory", disposalHistory);
+        //DisposalHistoryDto disposalHistory = disposalHistoryService.getHistory(disposalHistoryId);
+        //model.addAttribute("disposalHistory", disposalHistory);
+        
+        //view 확인용 더미 데이터
+        DisposalHistoryDto dummy = DisposalHistoryDto.builder()
+                .disposalHistoryId(disposalHistoryId)
+                .aiPrediction("small_item")
+                .regionGu("강남구")
+                .createdAt("2025-07-20")
+                .finalItem("책꽂이")
+                .itemName("2단 책꽂이")
+                .minPrice("2000")
+                .maxPrice("5000")
+                .reportUrl("https://example.com/report")
+                .disposalImgUrl("https://via.placeholder.com/400x300.png?text=Sample+Image") // 테스트용 이미지
+                .build();
+
+        model.addAttribute("disposalHistory", dummy);
         return "/disposal/disposalResult";
     }
 
